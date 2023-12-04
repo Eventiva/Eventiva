a367cbaa5fca82a2036873e1ab250834b969ae85
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2023-12-04 21:53:05 +0000

    :fire: dep(common): remove rush config and remove requirement
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

--- common/scripts/preinstall.js
+++
@@ -1,6 +0,0 @@
-if (!/pnpm/.test(process.env.npm_execpath || '')) {
-  console.warn(
-    `\u001b[33mThis repository requires using pnpm as the package manager for scripts to work properly.\u001b[39m\n` + `Please use \u001b[33mnpm install -g pnpm\u001b[39m to install locally or use the preconfigured development containers\n`
-  )
-  process.exit(1)
-}
