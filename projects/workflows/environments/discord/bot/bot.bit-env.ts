fbabad40a0218a9ef41dd8b0ac0e53aa767e60e5
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2023-12-11 00:41:18 +0000

    ✨ feat(bot): add Discord bot environment
    The commit adds a new file, `bot.bit-env.ts`, which extends the Bit official NodeJS environment. The new environment provides support for compiling TypeScript, linting with ESLint, formatting with Prettier, and building components. It also includes a pipeline to run TypeScript and ESLint tasks for component builds. The purpose of this addition is to enhance the development experience for creating Discord bots.
    
    The commit includes additional changes to create configuration files for ESLint, Jest, Prettier, and TypeScript. These files enable customization and configuration for the respective tools used in the environment.
    
    Context:
    The new environment is built as a reusable development environment for creating NodeJS modules, entities, and apps. It provides a set of standardized tooling and processes to improve development efficiency and code quality.
    
    Note:
    This commit includes commented out code for setting up Jest as the test runner, which is currently disabled.
    
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

---
+++ projects/workflows/environments/discord/bot/bot.bit-env.ts
@@ -0,0 +1,79 @@
+/**
+ * this env extends the Bit official NodeJS environment.
+ * learn more: https://bit.cloud/bitdev/node/node-env
+ */
+import { NodeEnv } from '@bitdev/node.node-env';
+import { Compiler } from "@teambit/compiler";
+import { EnvHandler } from "@teambit/envs";
+import { Pipeline } from '@teambit/builder';
+import {
+ TypescriptCompiler,
+ resolveTypes,
+ TypescriptTask,
+} from "@teambit/typescript.typescript-compiler";
+import { ESLintLinter, EslintTask } from "@teambit/defender.eslint-linter";
+import { JestTask, JestTester } from "@teambit/defender.jest-tester";
+import { PrettierFormatter } from "@teambit/defender.prettier-formatter";
+import { Tester } from "@teambit/tester";
+
+export class Bot extends NodeEnv {
+  /* shorthand name for the environment */
+  name = "bot";
+
+  /* the compiler to use during development */
+  compiler(): EnvHandler<Compiler> {
+    return TypescriptCompiler.from({
+      tsconfig: this.tsconfigPath,
+      types: resolveTypes(__dirname, [this.tsTypesPath]),
+    });
+  }
+
+  /* the test runner to use during development */
+  // tester(): EnvHandler<Tester> {
+  //   return JestTester.from({
+  //     config: this.jestConfigPath,
+  //   });
+  // }
+
+  /* the linter to use during development */
+  linter() {
+    return ESLintLinter.from({
+      tsconfig: this.tsconfigPath,
+      configPath: this.eslintConfigPath,
+      pluginsPath: __dirname,
+      extensions: this.eslintExtensions,
+    });
+  }
+
+  /**
+   * the formatter to use during development
+   * (source files are not formatted as part of the components' build)
+   */
+  formatter() {
+    return PrettierFormatter.from({
+      configPath: this.prettierConfigPath,
+    });
+  }
+
+  /**
+   * a set of processes to be performed before a component is snapped, during its build phase
+   * @see https://bit.dev/docs/node-env/build-pipelines
+   */
+  build() {
+    return Pipeline.from([
+      TypescriptTask.from({
+        tsconfig: this.tsconfigPath,
+        types: resolveTypes(__dirname, [this.tsTypesPath]),
+      }),
+      EslintTask.from({
+        tsconfig: this.tsconfigPath,
+        configPath: this.eslintConfigPath,
+        pluginsPath: __dirname,
+        extensions: this.eslintExtensions,
+      }),
+      // JestTask.from({ config: this.jestConfigPath }),
+    ]);
+  }
+}
+
+export default new Bot();
---
+++ projects/workflows/environments/discord/bot/config/eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+      extends: [require.resolve('@teambit/react.react-env/config/eslintrc')],
+      // env: {
+      //   mocha: true
+      // }
+    };
