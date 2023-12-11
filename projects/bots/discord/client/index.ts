1fb21a61d0dddf21e4fd1bb4f45216ba519f6ca3
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2023-12-11 00:40:48 +0000

    feat: add client app-root and bit-app
    This commit adds the `client.app-root.ts` and `client.bit-app.tsx` files to the project. The `client.app-root.ts` file contains the `createClient` function, which initializes and runs the Discord client. The `client.bit-app.tsx` file configures the Bit application for the client module.
    
    The new additions provide the foundation for creating and running the Discord client, enabling further development of the client functionality.
    
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

---
+++ projects/bots/discord/client/index.ts
@@ -0,0 +1,41 @@
+/**
+ * @format
+ * -----
+ * Project: @eventiva/eventiva
+ * File: index.ts
+ * Path: \projects\bots\discord\client\index.ts
+ * Created Date: Sunday, December 10th 2023
+ * Author: Jonathan Stevens (Email: jonathan.stevens@eventiva.co.uk, Github: https://github.com/TGTGamer)
+ * -----
+ * Contributing: Please read through our contributing guidelines. Included are directions for opening
+ * issues, coding standards, and notes on development. These can be found at https://github.com/eventiva/eventiva/blob/develop/CONTRIBUTING.md
+ * 
+ * Code of Conduct: This project abides by the Contributor Covenant, version 2.0. Please interact in ways that contribute to an open,
+ * welcoming, diverse, inclusive, and healthy community. Our Code of Conduct can be found at https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
+ * -----
+ * Copyright (c) 2023 Eventiva - All Rights Reserved
+ * LICENSE: GNU General Public License v3.0 only (GPL-3.0)
+ * -----
+ * This program has been provided under confidence of the copyright holder and is 
+ * licensed for copying, distribution and modification under the terms of
+ * the GNU General Public License v3.0 only (GPL-3.0) published as the License,
+ * or (at your option) any later version of this license.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v3.0 only for more details.
+ * 
+ * You should have received a copy of the GNU General Public License v3.0 only
+ * along with this program. If not, please write to: jonathan.stevens@eventiva.co.uk,
+ * or see https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ * 
+ * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
+ * -----
+ * Last Modified: 10-12-2023
+ * By: Jonathan Stevens (Email: jonathan.stevens@eventiva.co.uk, Github: https://github.com/TGTGamer)
+ * Current Version: 0.0.0
+ */
+
+export { Client, ClientOptions } from './client.js';
+export { default as ClientApp } from './client.bit-app.js'; 
