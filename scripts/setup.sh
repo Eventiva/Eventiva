26b574f32166f589e3cde3066857a736e7dc8165
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2023-12-14 00:09:50 +0000

    🔧 chore(devcontainer.json): add initializeCommand to run setup.sh script during container initialization 📝 docs(setup.sh): create setup.sh script to generate .env file with default environment variables for Docker Compose

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+# create a env file for docker-compose
+
+# test if the env file exists at the root of the project
+if [ ! -f .env ]; then
+
+touch .env
+echo "ERPNEXT_VERSION=v15.4.0
+DB_PASSWORD=databasePassword
+DB_HOST=db
+DB_PORT=3306
+REDIS_CACHE=redis-cache:6379
+REDIS_QUEUE=redis-queue:6379" >> .env
+
+fi
+
