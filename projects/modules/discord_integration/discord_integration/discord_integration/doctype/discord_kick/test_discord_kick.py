144849e8fc73516fe639a393ddec1ae343426d6b
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2023-12-19 14:59:58 +0000

    git subrepo clone git@github.com:Eventiva/discord_integration.git projects/modules/discord_integration
    subrepo:
      subdir:   "projects/modules/discord_integration"
      merged:   "9ab65f9"
    upstream:
      origin:   "git@github.com:Eventiva/discord_integration.git"
      branch:   "develop"
      commit:   "9ab65f9"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

---
+++ projects/modules/discord_integration/discord_integration/discord_integration/doctype/discord_kick/test_discord_kick.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Eventiva and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDiscordKick(FrappeTestCase):
+	pass
---
+++ projects/modules/discord_integration/discord_integration/discord_integration/doctype/discord_server/discord_server.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Eventiva and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class DiscordServer(NestedSet):
+	pass
