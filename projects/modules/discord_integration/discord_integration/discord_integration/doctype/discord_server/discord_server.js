44eb9f6bc0c15097b1972a21dbe00fb638332850
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2023-12-20 22:07:56 +0000

    feat: Overhaul Discord integration module
    - Devcontainer configuration disabled to streamline setup.
    - Field renaming for consistency in Discord Channel DocType.
    - Added comprehensive fields for role permissions in Discord Role DocType.
    - Overhauled Discord Server DocType structure, introducing timeouts and separate moderation tabs.
    - Implemented new DocTypes for managing Discord server kicks and timeouts.
    - Extended permissions and roles across various Discord DocTypes to enhance management capabilities.
    - Added exhaustive documentation and meta data to `discord_server.js`.
    - Introduced tabs within Discord User for better structuring of server and moderation information.
    
    This update significantly expands the management features and structures for Discord within the system, with a focus on moderation and permissions handling, along with an updated setup process.
    
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

--- projects/modules/discord_integration/discord_integration/discord_integration/doctype/discord_server/discord_server.js
+++ projects/modules/discord_integration/discord_integration/discord_integration/doctype/discord_server/discord_server.js
@@ -1,5 +1,52 @@
+/**
+ * @format
+ * -----
+ * Project: discord_server
+ * File: discord_server.js
+ * Path: /discord_server.js
+ * Created Date: Sunday, December 17th 2023
+ * Author: Jonathan Stevens (Email: jonathan@resnovas.com, Github: https://github.com/TGTGamer)
+ * -----
+ * Contributing: Please read through our contributing guidelines. Included are directions for opening
+ * issues, coding standards, and notes on development. These can be found at https://github.com/discord_server/blob/develop/CONTRIBUTING.md
+ * 
+ * Code of Conduct: This project abides by the Contributor Covenant, version 2.0. Please interact in ways that contribute to an open,
+ * welcoming, diverse, inclusive, and healthy community. Our Code of Conduct can be found at https://github.com/discord_server/blob/develop/CODE_OF_CONDUCT.md
+ * -----
+ * Copyright (c) 2023 Resnovas - All Rights Reserved
+ * LICENSE: Creative Commons Zero v1.0 Universal (CC0-1.0)
+ * -----
+ * This program has been provided under confidence of the copyright holder and is 
+ * licensed for copying, distribution and modification under the terms of
+ * the Creative Commons Zero v1.0 Universal (CC0-1.0) published as the License,
+ * or (at your option) any later version of this license.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Creative Commons Zero v1.0 Universal for more details.
+ * 
+ * You should have received a copy of the Creative Commons Zero v1.0 Universal
+ * along with this program. If not, please write to: jonathan@resnovas.com,
+ * or see https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ * 
+ * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
+ * -----
+ * Last Modified: 20-12-2023
+ * By: Jonathan Stevens (Email: jonathan@resnovas.com, Github: https://github.com/TGTGamer)
+ */
+
 // Copyright (c) 2023, Eventiva and contributors
 // For license information, please see license.txt
+
+frappe.ui.form.on("Discord Server", "onload", function (frm) {
+	frm.set_query("timeouts", function () {
+		return {
+			filters: {
+			}
+		};
+	});
+});
 
 // frappe.ui.form.on("Discord Server", {
 // 	refresh(frm) {
