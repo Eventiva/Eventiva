5e49cf7b0f90e7c209df3a88c03c7e43bc03b1ff
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-01-07 23:35:52 +0000

    :sparkles: feat(discord bot): Add support and ping commands, interaction event handling
    Introduce new commands for interaction-based support and ping utilities. Implement essential event handlers to enable Discord interaction responses, including handling command execution and user messages. Expand internationalization capabilities by defining type augmentations for custom i18n resources. Remove obsolete code and metadata comments to streamline project structure.
    
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

--- projects/bots/discord/client/commands/utility/ping.ts
+++ projects/bots/discord/client/commands/utility/ping.ts
@@ -2,9 +2,9 @@
  * @format
  * -----
  * Project: @eventiva/eventiva
- * File: index.ts
- * Path: \projects\bots\discord\client\index.ts
- * Created Date: Tuesday, December 12th 2023
+ * File: ping.ts
+ * Path: \projects\bots\discord\client\commands\utility\ping.ts
+ * Created Date: Wednesday, January 3rd 2024
  * Author: Jonathan Stevens (Email: jonathan.stevens@eventiva.co.uk, Github: https://github.com/TGTGamer)
  * -----
  * Contributing: Please read through our contributing guidelines. Included are directions for opening
@@ -13,7 +13,7 @@
  * Code of Conduct: This project abides by the Contributor Covenant, version 2.0. Please interact in ways that contribute to an open,
  * welcoming, diverse, inclusive, and healthy community. Our Code of Conduct can be found at https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
  * -----
- * Copyright (c) 2023 - 2024 Eventiva - All Rights Reserved
+ * Copyright (c) 2024 Eventiva - All Rights Reserved
  * LICENSE: GNU General Public License v3.0 only (GPL-3.0)
  * -----
  * This program has been provided under confidence of the copyright holder and is
@@ -33,5 +33,26 @@
  * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
  */
 
-export { Client, ClientOptions } from './client.js';
-export { default as ClientApp } from './client.bit-app.js';
+import { SlashCommandBuilder } from 'discord.js';
+import type { Command } from '../../client';
+
+/**
+ * A Command resource that represents a slash command. It has a type of 'slash' and data that is a new instance of the SlashCommandBuilder class. The command's name is 'ping' and its description is 'Replies with Pong!'. The execute method is an async function that accepts the 'this' context and an interaction parameter. When executed, it replies with 'Pong!' to the interaction.
+ * @author Jonathan Stevens (@TGTGamer)
+ *
+ * @type {Command}
+ */
+export const resource: Command = {
+  type: 'slash',
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute(this, interaction) {
+    await interaction.reply('Pong!');
+  },
+  async message(this, message) {
+    await message.reply('Pong!');
+  },
+};
+
+export default resource;
--- projects/bots/discord/client/types/i18next.d.ts
+++ projects/bots/discord/client/types/i18next.d.ts
@@ -2,9 +2,9 @@
  * @format
  * -----
  * Project: @eventiva/eventiva
- * File: index.ts
- * Path: \projects\bots\discord\client\index.ts
- * Created Date: Tuesday, December 12th 2023
+ * File: i18next.d.ts
+ * Path: \projects\bots\discord\client\types\i18next.d.ts
+ * Created Date: Friday, January 5th 2024
  * Author: Jonathan Stevens (Email: jonathan.stevens@eventiva.co.uk, Github: https://github.com/TGTGamer)
  * -----
  * Contributing: Please read through our contributing guidelines. Included are directions for opening
@@ -13,7 +13,7 @@
  * Code of Conduct: This project abides by the Contributor Covenant, version 2.0. Please interact in ways that contribute to an open,
  * welcoming, diverse, inclusive, and healthy community. Our Code of Conduct can be found at https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
  * -----
- * Copyright (c) 2023 - 2024 Eventiva - All Rights Reserved
+ * Copyright (c) 2024 Eventiva - All Rights Reserved
  * LICENSE: GNU General Public License v3.0 only (GPL-3.0)
  * -----
  * This program has been provided under confidence of the copyright holder and is
@@ -33,5 +33,11 @@
  * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
  */
 
-export { Client, ClientOptions } from './client.js';
-export { default as ClientApp } from './client.bit-app.js';
+import { resources, defaultNS } from '../client';
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    defaultNS: typeof defaultNS;
+    resources: typeof resources['en'];
+  }
+}
