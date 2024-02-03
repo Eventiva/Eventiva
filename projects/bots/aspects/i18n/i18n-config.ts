12288d472b240289cd83b74426e5f81f8d5e5b55
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-02-03 02:41:14 +0000

    📝 chore(i18n-config.ts): update I18NConfig type to extend InitOptions type for better configuration of internationalization
    🔀 refactor(i18n-config.ts): reformat file and remove unnecessary comments and headers

--- projects/bots/aspects/i18n/i18n-config.ts
+++ projects/bots/aspects/i18n/i18n-config.ts
@@ -1,6 +1,52 @@
+/**
+* @format
+* -----
+* Project: @eventiva/eventiva
+* File: i18n-config.ts
+* Path: \projects\bots\aspects\i18n\i18n-config.ts
+* Created Date: Monday, January 29th 2024
+* Author: Jonathan Stevens, jonathan@resnovas.com
+* Github: https://github.com/TGTGamer
+* -----
+* Contributing: Please read through our contributing guidelines.
+* Included are directions for opening issues, coding standards,
+* and notes on development. These can be found at
+* https://github.com/eventiva/eventiva/blob/develop/CONTRIBUTING.md
+* -----
+* Code of Conduct: This project abides by the Contributor Covenant, v2.0
+* Please interact in ways that contribute to an open, welcoming, diverse,
+* inclusive, and healthy community. Our Code of Conduct can be found at
+* https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
+* -----
+* Copyright (c) 2024 Resnovas - All Rights Reserved
+* LICENSE: Creative Commons Zero v1.0 Universal (CC0-1.0)
+* -----
+* This program has been provided under confidence of the copyright holder and
+* is licensed for copying, distribution and modification under the terms of
+* the Creative Commons Zero v1.0 Universal (CC0-1.0) published as the License,
+* or (at your option) any later version of this license.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* Creative Commons Zero v1.0 Universal for more details.
+* You should have received a copy of the Creative Commons Zero v1.0 Universal
+* along with this program. If not, please write to: jonathan@resnovas.com,
+* or see https://creativecommons.org/publicdomain/zero/1.0/legalcode
+* -----
+* DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE
+*/
 
-// use this type for your aspect config.
+
+import { InitOptions } from "i18next";
+
+/**
+ * Use this type for configuring internationalization (i18n). It extends the InitOptions type.
+ * @author Jonathan Stevens (@TGTGamer)
+ *
+ * @export
+ * @typedef {I18NConfig}
+ */
 export type I18NConfig = {
 
-};
-
+} & InitOptions;
+ 
