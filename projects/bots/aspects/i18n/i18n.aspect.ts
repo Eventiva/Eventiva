fa3e00478fc4b4fd9c3f62398ff5e59b6a60ecc8
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-02-03 02:41:25 +0000

    refactor: remove unnecessary comments and fix formatting
    📝 chore(i18n.aspect.ts): update file header with project information, author, and contributing guidelines
    
    🔒 chore(i18n.aspect.ts): update license information and add code of conduct link

--- projects/bots/aspects/i18n/i18n.aspect.ts
+++ projects/bots/aspects/i18n/i18n.aspect.ts
@@ -1,8 +1,51 @@
+/**
+* @format
+* -----
+* Project: @eventiva/eventiva
+* File: i18n.aspect.ts
+* Path: \projects\bots\aspects\i18n\i18n.aspect.ts
+* Created Date: Monday, January 29th 2024
+* Author: Jonathan Stevens, jonathan@resnovas.com
+* Github: https://github.com/TGTGamer
+* -----
+* Contributing: Please read through our contributing guidelines.
+* Included are directions for opening issues, coding standards,
+* and notes on development. These can be found at
+* https://github.com/eventiva/eventiva/blob/develop/CONTRIBUTING.md
+* -----
+* Code of Conduct: This project abides by the Contributor Covenant, v2.0
+* Please interact in ways that contribute to an open, welcoming, diverse,
+* inclusive, and healthy community. Our Code of Conduct can be found at
+* https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
+* -----
+* Copyright (c) 2024 Resnovas - All Rights Reserved
+* LICENSE: Creative Commons Zero v1.0 Universal (CC0-1.0)
+* -----
+* This program has been provided under confidence of the copyright holder and
+* is licensed for copying, distribution and modification under the terms of
+* the Creative Commons Zero v1.0 Universal (CC0-1.0) published as the License,
+* or (at your option) any later version of this license.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* Creative Commons Zero v1.0 Universal for more details.
+* You should have received a copy of the Creative Commons Zero v1.0 Universal
+* along with this program. If not, please write to: jonathan@resnovas.com,
+* or see https://creativecommons.org/publicdomain/zero/1.0/legalcode
+* -----
+* DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE
+*/
+
 import { Aspect } from '@bitdev/harmony.harmony';
 
+/**
+ * The I18NAspect is a built-in aspect that provides internationalization capabilities to bots. It allows bots to dynamically load and manage multiple language resources and provides methods for translating messages.
+ * @author Jonathan Stevens (@TGTGamer)
+ *
+ * @type {*}
+ */
 export const I18NAspect = Aspect.create({
   id: 'eventiva.bots/aspects/i18n'
 });
 
 export default I18NAspect;
-    
