c69de3bd66280bd634df489147db2d0eb6b5f031
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-02-04 23:39:03 +0000

    refactor: remove unused message-related files and code
    The following files were deleted:
    - `projects/bots/discord/events/message/index.ts`
    - `projects/bots/discord/events/message/message-config.ts`
    - `projects/bots/discord/events/message/message.aspect.ts`
    - `projects/bots/discord/events/message/message.docs.mdx`
    - `projects/bots/discord/events/message/message.node.runtime.ts`
    - `projects/bots/discord/events/message/message.node.spec.ts`
    
    These files were removed because they were no longer needed and were not being used in the project. Removing them helps to declutter the codebase and improve maintainability.

--- projects/bots/discord/events/message/index.ts
+++
@@ -1,6 +0,0 @@
-import { MessageAspect } from './message.aspect.js';
-
-export type { MessageNode } from './message.node.runtime.js';
-
-export default MessageAspect;
-export { MessageAspect };
--- projects/bots/discord/events/message/message.aspect.ts
+++
@@ -1,8 +0,0 @@
-import { Aspect } from '@bitdev/harmony.harmony';
-
-export const MessageAspect = Aspect.create({
-  id: 'eventiva.bots/discord/events/message'
-});
-
-export default MessageAspect;
-    
