3ceae237537e9b30ca535a9a6abad48c1e3fab0d
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-02-04 23:39:28 +0000

    Update licenses and add project information
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

--- projects/bots/aspects/atlassian-customer-support/atlassian-customer-support.node.runtime.ts
+++ projects/bots/aspects/atlassian-customer-support/atlassian-customer-support.node.runtime.ts
@@ -19,19 +19,28 @@
 * https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
 * -----
 * Copyright (c) 2024 Resnovas - All Rights Reserved
-* LICENSE: Creative Commons Zero v1.0 Universal (CC0-1.0)
+* LICENSE: GNU General Public License v2.0 or later (GPL-2.0-or-later)
 * -----
 * This program has been provided under confidence of the copyright holder and
-* is licensed for copying, distribution and modification under the terms of
-* the Creative Commons Zero v1.0 Universal (CC0-1.0) published as the License,
+* is licensed for copying, distribution and modification under the terms
+* of the GNU General Public License v2.0 or later (GPL-2.0-or-later) published as the License,
 * or (at your option) any later version of this license.
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* Creative Commons Zero v1.0 Universal for more details.
-* You should have received a copy of the Creative Commons Zero v1.0 Universal
+* GNU General Public License v2.0 or later for more details.
+* You should have received a copy of the GNU General Public License v2.0 or later
 * along with this program. If not, please write to: jonathan@resnovas.com,
-* or see https://creativecommons.org/publicdomain/zero/1.0/legalcode
+* or see https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+* -----
+* This project abides by the GPL Cooperation Commitment.
+* Before filing or continuing to prosecute any legal proceeding or claim
+* (other than a Defensive Action) arising from termination of a Covered
+* License, we commit to extend to the person or entity ('you') accused
+* of violating the Covered License the following provisions regarding
+* cure and reinstatement, taken from GPL version 3.
+* For further details on the GPL Cooperation Commitment please visit
+* the official website: https://gplcc.github.io/gplcc/
 * -----
 * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE
 */
@@ -40,8 +49,30 @@ import type { AtlassianCustomerSupportConfig } from './atlassian-customer-suppor
 import { DiscordjsAspect, type DiscordjsNode } from '@eventiva/bots.aspects.discordjs';
 
 
+/**
+ * This class represents an Atlassian Customer Support node. It is responsible for providing support for Atlassian customers through Discord. It depends on the DiscordjsNode class and requires a config object of type AtlassianCustomerSupportConfig.
+ * @author Jonathan Stevens (@TGTGamer)
+ *
+ * @export
+ * @class
+ */
 export class AtlassianCustomerSupportNode {
+  /**
+   * A private property that holds an instance of the Discord.js client for interacting with the Discord API.
+   * @author Jonathan Stevens (@TGTGamer)
+   *
+   * @private
+   */
   private discordjs: DiscordjsNode
+  /**
+   * Creates an instance of AtlassianCustomerSupportNode.
+   * @author Jonathan Stevens (@TGTGamer)
+   *
+   * @constructor
+   * @param param0 The Discord.js module.
+   * @param param0.discordjs An instance of the Discord.js Node module.
+   * @param config The configuration for Atlassian Customer Support.
+   */
   constructor(
     [discordjs]: [DiscordjsNode],
     private config: AtlassianCustomerSupportConfig,
@@ -49,10 +80,33 @@ export class AtlassianCustomerSupportNode {
     this.discordjs = discordjs
   }
   
+  /**
+   * An array of dependencies for the static property. It includes only DiscordjsAspect.
+   * @author Jonathan Stevens (@TGTGamer)
+   *
+   * @static
+   */
   static dependencies = [DiscordjsAspect];
 
+  /**
+   * A static property that represents the default configuration for the AtlassianCustomerSupportConfig. It is an empty object by default.
+   * @author Jonathan Stevens (@TGTGamer)
+   *
+   * @static
+   */
   static defaultConfig: AtlassianCustomerSupportConfig = {};
 
+  /**
+   * Creates a new instance of AtlassianCustomerSupportNode and returns it.
+   * @author Jonathan Stevens (@TGTGamer)
+   *
+   * @static
+   * @async
+   * @param param0 An instance of Discord.js client
+   * @param param0.discordjs An instance of Discord.js client
+   * @param config The configuration object for the Atlassian Customer Support module
+   * @returns This function is a provider function that creates a new instance of the AtlassianCustomerSupportNode class with the given DiscordjsNode instance and config. It then calls the listCommands method on the AtlassianCustomerSupportNode instance. Finally, it returns the created AtlassianCustomerSupportNode instance.
+   */
   static async provider(
     [discordjs]: [DiscordjsNode],
     config: AtlassianCustomerSupportConfig,
