2e3cbe4b205b19aa2dc7cba0edeb971e73c689b3
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-02-07 21:05:21 +0000

    feat: bit todo manager
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

---
+++ projects/workflows/bit/todo/todo.aspect.ts
@@ -0,0 +1,8 @@
+import { Aspect } from '@teambit/harmony';
+
+/**
+ * register the aspect ID for this aspect
+ *  */
+export const TodoAspect = Aspect.create({
+  id: 'eventiva.workflows/bit/todo',
+});
