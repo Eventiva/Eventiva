159f6fdb5a124c645e54f300253ff16d7bb42d7a
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-02-18 16:52:18 +0000

    feat: create segment aspect
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

---
+++ projects/central/aspects/segment/segment.aspect.ts
@@ -0,0 +1,12 @@
+import { Aspect } from '@bitdev/harmony.harmony';
+
+/**
+ * Create an aspect with the id 'eventiva.central/aspects/segment'
+ * @author Jonathan Stevens (@TGTGamer)
+ */
+export const SegmentAspect = Aspect.create({
+  id: 'eventiva.central/aspects/segment'
+});
+
+export default SegmentAspect;
+    
