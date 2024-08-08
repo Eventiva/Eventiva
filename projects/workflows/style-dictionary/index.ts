60a3f81fb7cb582a69baa8e5f92e90e64009d6ff
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-08-09 00:32:24 +0100

    fix: correct export naming in style-dictionary module
    The export was incorrectly named, causing issues in module imports. Changed export from 'styleDictionary' to 'StyleDictionary' to resolve this.
    
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

--- projects/workflows/style-dictionary/index.ts
+++ projects/workflows/style-dictionary/index.ts
@@ -1,7 +1,7 @@
 /*
  * Project: Eventiva
  * File: index.ts
- * Last Modified: 09/08/2024, 00:06
+ * Last Modified: 09/08/2024, 00:31
  *
  * Contributing: Please read through our contributing guidelines.
  * Included are directions for opening issues, coding standards,
@@ -34,4 +34,4 @@
  * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE
  */
 
-export { styleDictionary } from './style-dictionary.js'
+export { StyleDictionary } from './style-dictionary.js'
