0847325069f8dba0a94d172b3199e712982f4cba
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-08-09 00:05:45 +0100

    feat: add initial Tailwind configuration for project
    Introduce a comprehensive Tailwind setup including configurations, presets, and global styles. This setup enables themes, handles multiple file types, and includes necessary documentation.
    
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

---
+++ projects/envs/config/tailwind/tailwind.composition.tsx
@@ -0,0 +1,63 @@
+/*
+ * Project: Eventiva
+ * File: tailwind.composition.tsx
+ * Last Modified: 08/08/2024, 20:08
+ *
+ * Contributing: Please read through our contributing guidelines.
+ * Included are directions for opening issues, coding standards,
+ * and notes on development. These can be found at
+ * https://github.com/eventiva/eventiva/blob/develop/CONTRIBUTING.md
+ *
+ * Code of Conduct: This project abides by the Contributor Covenant, v2.0
+ * Please interact in ways that contribute to an open, welcoming, diverse,
+ * inclusive, and healthy community. Our Code of Conduct can be found at
+ * https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
+ *
+ * Copyright (c) 2024 Eventiva Ltd. All Rights Reserved
+ * LICENSE: Functional Source License, Version 1.1, MIT Future License (FSL-1.1-MIT)
+ *
+ * This program has been provided under confidence of the copyright holder and is licensed for copying, distribution
+ * and modification under the terms of the Functional Source License, Version 1.1, MIT Future License (FSL-1.1-MIT)
+ * published as the License, or (at your option) any later version of this license. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the Functional Source License, Version 1.1, MIT Future License for more
+ * details. You should have received a copy of the Functional Source License, Version 1.1, MIT Future License along
+ * with this program. If not, please write to: licensing@eventiva.co.uk, see the official website
+ * https://fsl.software/ or Review the GitHub repository https://github.com/getsentry/fsl.software/
+ *
+ * This project abides the Eventiva Cooperation Commitment. Adapted from the GPL Cooperation Commitment (GPLCC). Before
+ * filing or continuing to prosecute any legal proceeding or claim (other than a Defensive Action) arising from
+ * termination of a Covered License, we commit to adhering to the Eventiva Cooperation Commitment. You should have
+ * received a copy of the Eventiva Cooperation Commitment along with this program. If not, please write to:
+ * licensing@eventiva.co.uk, or see https://eventiva.co.uk/licensing/ecc
+ *
+ * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE
+ */
+
+import React from 'react';
+
+export const BasicComposition = () => {
+    return (
+            <svg
+                    width="256"
+                    height="154"
+                    xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="xMidYMid"
+                    style={{
+                        margin: '0 auto',
+                        padding: '1rem 1rem',
+                    }}
+            >
+                <defs>
+                    <linearGradient x1="-2.778%" y1="32%" x2="100%" y2="67.556%" id="a">
+                        <stop stop-color="#2298BD" offset="0%"/>
+                        <stop stop-color="#0ED7B5" offset="100%"/>
+                    </linearGradient>
+                </defs>
+                <path
+                        d="M128 0C93.867 0 72.533 17.067 64 51.2 76.8 34.133 91.733 27.733 108.8 32c9.737 2.434 16.697 9.499 24.401 17.318C145.751 62.057 160.275 76.8 192 76.8c34.133 0 55.467-17.067 64-51.2-12.8 17.067-27.733 23.467-44.8 19.2-9.737-2.434-16.697-9.499-24.401-17.318C174.249 14.743 159.725 0 128 0ZM64 76.8C29.867 76.8 8.533 93.867 0 128c12.8-17.067 27.733-23.467 44.8-19.2 9.737 2.434 16.697 9.499 24.401 17.318C81.751 138.857 96.275 153.6 128 153.6c34.133 0 55.467-17.067 64-51.2-12.8 17.067-27.733 23.467-44.8 19.2-9.737-2.434-16.697-9.499-24.401-17.318C110.249 91.543 95.725 76.8 64 76.8Z"
+                        fill="url(#a)"
+                />
+            </svg>
+    );
+};
---
+++ projects/envs/config/tailwind/tailwind.config.ts
@@ -0,0 +1,119 @@
+/*
+ * Project: Eventiva
+ * File: tailwind.config.ts
+ * Last Modified: 08/08/2024, 20:24
+ *
+ * Contributing: Please read through our contributing guidelines.
+ * Included are directions for opening issues, coding standards,
+ * and notes on development. These can be found at
+ * https://github.com/eventiva/eventiva/blob/develop/CONTRIBUTING.md
+ *
+ * Code of Conduct: This project abides by the Contributor Covenant, v2.0
+ * Please interact in ways that contribute to an open, welcoming, diverse,
+ * inclusive, and healthy community. Our Code of Conduct can be found at
+ * https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
+ *
+ * Copyright (c) 2024 Eventiva Ltd. All Rights Reserved
+ * LICENSE: Functional Source License, Version 1.1, MIT Future License (FSL-1.1-MIT)
+ *
+ * This program has been provided under confidence of the copyright holder and is licensed for copying, distribution
+ * and modification under the terms of the Functional Source License, Version 1.1, MIT Future License (FSL-1.1-MIT)
+ * published as the License, or (at your option) any later version of this license. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the Functional Source License, Version 1.1, MIT Future License for more
+ * details. You should have received a copy of the Functional Source License, Version 1.1, MIT Future License along
+ * with this program. If not, please write to: licensing@eventiva.co.uk, see the official website
+ * https://fsl.software/ or Review the GitHub repository https://github.com/getsentry/fsl.software/
+ *
+ * This project abides the Eventiva Cooperation Commitment. Adapted from the GPL Cooperation Commitment (GPLCC). Before
+ * filing or continuing to prosecute any legal proceeding or claim (other than a Defensive Action) arising from
+ * termination of a Covered License, we commit to adhering to the Eventiva Cooperation Commitment. You should have
+ * received a copy of the Eventiva Cooperation Commitment along with this program. If not, please write to:
+ * licensing@eventiva.co.uk, or see https://eventiva.co.uk/licensing/ecc
+ *
+ * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE
+ */
+
+import { defineConfig } from '@frontend/tailwind.utils.define-config'
+import { createRequire } from 'node:module'
+import defaultTheme from 'tailwindcss/defaultTheme.js'
+import type { Config as TailwindConfig } from 'tailwindcss/types/config.js'
+
+const cjsRequire = createRequire( import.meta.url )
+const { fontFamily } = defaultTheme
+
+export const config: TailwindConfig = defineConfig( {
+    darkMode: [ 'class' ],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px'
+            }
+        },
+        extend: {
+            colors: {
+                //   shadcn-ui defaults
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))'
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))'
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))'
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))'
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))'
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))'
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))'
+                }
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            fontFamily: {
+                sans: [ 'var(--font-sans)', ...fontFamily.sans ]
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: '0' },
+                    to: { height: 'var(--radix-accordion-content-height)' }
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: '0' }
+                }
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out'
+            }
+        }
+    },
+    plugins: [ cjsRequire( 'tailwindcss-animate' ) ]
+} )
+
+export default config
