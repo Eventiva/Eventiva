fbf76a8457863e7b3ccc700c66c8e7bd3eb8300c
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-09-06 08:33:56 +0100

    chore(license): switch to Fair Core License Version 1.0
    Replace existing Functional Source License with Fair Core License Version 1.0 in multiple files. Updated license details include new restrictions on license key functionality and aligned with the Eventiva Cooperation Commitment.
    
    Signed-off-by: Jonathan Stevens <jonathan.stevens@eventiva.co.uk>

--- projects/discordjs/discordjs/module.ts
+++ projects/discordjs/discordjs/module.ts
@@ -1,35 +1,34 @@
 /*
  * Project: Eventiva
  * File: module.ts
- * Last Modified: 28/08/2024, 18:01
+ * Last Modified: 06/09/2024, 08:12
  *
- * Contributing: Please read through our contributing guidelines.
- * Included are directions for opening issues, coding standards,
- * and notes on development. These can be found at
- * https://github.com/eventiva/eventiva/blob/develop/CONTRIBUTING.md
+ * Contributing: Please read through our contributing guidelines. Included are directions for opening issues, coding standards,
+ *  and notes on development. These can be found at https://github.com/eventiva/eventiva/blob/develop/CONTRIBUTING.md
  *
- * Code of Conduct: This project abides by the Contributor Covenant, v2.0
- * Please interact in ways that contribute to an open, welcoming, diverse,
- * inclusive, and healthy community. Our Code of Conduct can be found at
- * https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
+ * Code of Conduct: This project abides by the Contributor Covenant, v2.0. Please interact in ways that contribute to an open,
+ *  welcoming, diverse, inclusive, and healthy community. Our Code of Conduct can be found at
+ *  https://github.com/eventiva/eventiva/blob/develop/CODE_OF_CONDUCT.md
  *
  * Copyright (c) 2024 Eventiva Ltd. All Rights Reserved
- * LICENSE: Functional Source License, Version 1.1, MIT Future License (FSL-1.1-MIT)
+ * LICENSE: Fair Core License, Version 1.0, MIT Future License (FCL-1.0-MIT)
  *
- * This program has been provided under confidence of the copyright holder and is licensed for copying, distribution
- * and modification under the terms of the Functional Source License, Version 1.1, MIT Future License (FSL-1.1-MIT)
- * published as the License, or (at your option) any later version of this license. This program is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the Functional Source License, Version 1.1, MIT Future License for more
- * details. You should have received a copy of the Functional Source License, Version 1.1, MIT Future License along
- * with this program. If not, please write to: licensing@eventiva.co.uk, see the official website
- * https://fsl.software/ or Review the GitHub repository https://github.com/getsentry/fsl.software/
+ * This program has been provided under confidence of the copyright holder and is licensed for copying, distribution and
+ * modification under the terms of the Fair Core License, Version 1.0, MIT Future License (FCL-1.0-MIT) published as the License, or
+ *  (at your option) any later version of this license. You must not move, change, disable, or circumvent the license key functionality
+ *   in the Software; or modify any portion of the Software protected by the license key to: enable access to the protected
+ *   functionality without a valid license key; or remove the protected functionality.This program is distributed in the hope that it will
+ *   be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *   PARTICULAR PURPOSE. See the Fair Core License, Version 1.0, MIT Future License for more details. You should have received a
+ *   copy of the Fair Core License, Version 1.0, MIT Future License along with this program. If not, please write to:
+ *   licensing@eventiva.co.uk, see the official website https://fcl.dev/ or Review the GitHub repository
+ *   https://github.com/keygen-sh/fcl.dev/
  *
- * This project abides the Eventiva Cooperation Commitment. Adapted from the GPL Cooperation Commitment (GPLCC). Before
- * filing or continuing to prosecute any legal proceeding or claim (other than a Defensive Action) arising from
- * termination of a Covered License, we commit to adhering to the Eventiva Cooperation Commitment. You should have
- * received a copy of the Eventiva Cooperation Commitment along with this program. If not, please write to:
- * licensing@eventiva.co.uk, or see https://eventiva.co.uk/licensing/ecc
+ * This project abides the Eventiva Cooperation Commitment. Adapted from the GPL Cooperation Commitment (GPLCC). Before filing
+ *  or continuing to prosecute any legal proceeding or claim (other than a Defensive Action) arising from termination of a Covered
+ *  License, we commit to adhering to the Eventiva Cooperation Commitment. You should have received a copy of the Eventiva
+ *  Cooperation Commitment along with this program. If not, please write to: licensing@eventiva.co.uk, or see
+ *  https://eventiva.co.uk/licensing/ecc
  *
  * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE
  */
